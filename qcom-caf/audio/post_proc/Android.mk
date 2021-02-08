81fa2cdcbcc4b05ea37cf773725443dd6c671211
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-02-08 08:08:27 +0530

    rosy: hardcode pathmaps for HALs

--- qcom-caf/audio/post_proc/Android.mk
+++ qcom-caf/audio/post_proc/Android.mk
@@ -195,14 +195,14 @@ LOCAL_MODULE_OWNER := qti
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := \
-        $(call project-path-for,qcom-audio)/hal \
+        device/xiaomi/rosy/qcom-caf/audio/hal \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/audio \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
-        $(call project-path-for,qcom-audio)/hal/audio_extn \
+        device/xiaomi/rosy/qcom-caf/audio/hal/audio_extn \
         external/tinycompress/include \
         system/media/audio_utils/include
 
