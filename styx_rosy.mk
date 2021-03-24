8dce456bb8d2c07252fb23e349dd7d9562c8b55a
arulebin <ebinarul@protonmail.com>
2021-03-24 04:38:26 +0000

    Bringups for StyxOS

--- styx_rosy.mk
+++ styx_rosy.mk
@@ -9,19 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
-
-# Pixel customization
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Inherit some common Styx stuff
+$(call inherit-product, vendor/styx/config/common.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := pixys_rosy
+PRODUCT_NAME := styx_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
