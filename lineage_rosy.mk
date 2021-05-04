0f0c31319b2cae0ef8fff4926a45c41b01d27ae7
arulebin <arulebin995@gmail.com>
2021-05-04 16:16:28 +0000

    rosy: bringups for Cipheros official

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+# Inherit some common Cipher stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Pixel customization
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
@@ -21,7 +21,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := pixys_rosy
+PRODUCT_NAME := lineage_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
@@ -33,5 +33,10 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cipher
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.cipher.maintainer=Ebin
+TARGETFACEUNLOCK_SUPPORTED := true
+CIPHER_OFFICIAL := true
