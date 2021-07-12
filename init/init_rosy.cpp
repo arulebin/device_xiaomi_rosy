c21431c8e173ed691847f8af6afc2226112f830a
arulebin <ebinarul@protonmail.com>
2021-07-12 09:28:30 +0000

    rosy: update  to Redfin july fp

--- init/init_rosy.cpp
+++ init/init_rosy.cpp
@@ -94,6 +94,6 @@ void vendor_load_properties()
     property_override("ro.product.model", "Redmi 5");
     property_override("ro.build.product", "rosy");
     property_override("ro.product.device", "rosy");
-    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
-    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
+    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
 }
