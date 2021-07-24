e4dc88be34fe434a89f11ed862271bf9e76c24a5
arulebin <ebinarul@protonmail.com>
2021-07-24 09:02:17 +0000

    rosy: define board flags

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_SOONG_NAMESPACES += \
 QCOM_SOONG_NAMESPACE := \
     $(LOCAL_PATH)/qcom-caf
 
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
